e6603d11808d5bd5d48b635d8df0150e694f6f81
OFideliss <otavio-fidelis@outlook.com>
2023-11-06 20:36:51 -0300

    "06/11/2023"

---
+++ meu-primeiro-angular/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Importar FormsModule
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
+import { FormularioComponent } from './formulario/formulario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SobreComponent,
+    ContatoComponent,
+    FormularioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule // importar FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ meu-primeiro-angular/src/app/usuario.model.ts
@@ -0,0 +1,8 @@
+// src/app/usuario.model.ts
+export interface Usuario {
+
+    //declaração de variaveis em TypeScript
+    nome: string;
+    email: string;
+    // Adicione outros campos, se necessário
+}
